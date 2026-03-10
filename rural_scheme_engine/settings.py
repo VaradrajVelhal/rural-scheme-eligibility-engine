e39bff082bbf76f54860cc0c42a692d30cced593
Varadraj Velhal <varadrajvelhal@Varadrajs-MacBook-Air.local>
2026-03-10 10:04:56 +0530

    fix build.sh permission

--- rural_scheme_engine/settings.py
+++ rural_scheme_engine/settings.py
@@ -117,6 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 LOGIN_REDIRECT_URL = "/check/"
 LOGOUT_REDIRECT_URL = "/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
